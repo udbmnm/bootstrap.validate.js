92e2c08debfafc2f87afe0c4d97472882fca7dfa
@Life <udbmnm@163.com>
2014-07-11 16:14:15 +0800

    删除无用内容适应新项目

--- bootstrap.validate.js
+++ bootstrap.validate.js
@@ -1,69 +1,84 @@
+/*
+ * last update : 2014/07/11
+ * author Email : admin@f2e.be
+ * 
+ */
 "use strict";
 (function ($) {
- $.fn.Zsvalidation = function(opt){
-    var opt = $.extend({},defaults, opt),$self = $(this);
-    validate($self,opt);//绑定验证事件
-    refeshVcode(opt);//刷新验证码
+ var access = 0;
+ 
+ $.fn.validation = function(opt){
+ 	
+    var opt = $.extend({},defaults, opt),
+    	$self = $(this);
+    	
+    validate($self,opt);//blur触发时验证
+    
     $self.submit(function(e){
         e.preventDefault();
-        validate($self,opt,true); //提交批量时验证
-        var access = $(document).data('access');
+        validate($self,opt,1); //提交表单时需要批量时验证
+        
         if(access){
-            opt.success(opt);
+            opt.success();
         }else{
-            $('#u-code').prevAll('img').attr('src',opt.api.vcode+'?'+Math.random());
+            //$('#u-code').prevAll('img').attr('src',opt.api.vcode+'?'+Math.random());
             opt.failed();
         }
 
     });
+    
 };
-    var validate = function(obj,opt,sub){
-        var rules = opt.validRules;
-        var inputs = obj.find('input').filter(function(){
-            return $(this).attr('rule');
-        });
-        if(sub) {
-            inputs.each(function(){
-                return verify($(this),rules);    
+    var validate = function(eles,opt,isSubmit){
+        var rules = eles.validRules,
+        	$this = $(this),
+        	$inputs = eles.find('input').filter(function(){
+	            return $this.attr('rule');
+	        });
+        
+        if(isSubmit) {//如果是直接提交则全部验证
+            $inputs.each(function(){
+                verify($this,rules,opt);    
             });
-        }else{
-            inputs.on('blur',null,function(){
-                return verify($(this),rules);
+        }else{//单个验证
+            $inputs.on('blur',function(){
+                verify($this,rules,opt);
             });
         }
     };
-    //验证方法和dom操作 equalto
-    var verify = function(ele,rules){
-            //ele为一个jQuery对象
+    
+    //ele为一个jQuery对象
+    var verify = function(ele,rules,opt){
+    	//是否跳过该ele验证
         if(ele.attr('ignore') === '') return;
-        var r = ele.attr('rule')?ele.attr('rule').split(','):[],
-        value = ele.val(), 
-        access = false,
-        msg = ele.attr('msg'),
-        equalto = ele.attr('equalto'),
-        equalmsg = ele.attr('equalmsg'),
-        bool = true,
-        val = '';
-        //可能验证规则为多个，多个规则用“,”分割//感叹号说明需要相反的验证结果
-        $.each(r,function(i){
-            $.each(rules,function(j,v){
-                if(r[i].slice(0,1) === '!') {
-                    bool = false;
-                    val = r[i].slice(1);
+        //可能验证规则为多个，多个规则用“,”分割 ，感叹号说明需要取相反的验证结果
+        var eleRule = ele.attr('rule')?ele.attr('rule').split(','):[],
+        	msg = ele.attr('msg'),
+        	equalto = ele.attr('equalto'),//equalto最好写id，都懂的
+        	equalmsg = ele.attr('equalmsg'),
+        	value = ele.val(),
+        	isReverse = 0,
+        	ruleName = '';
+        
+        $.each(eleRule,function(i){
+            $.each(rules,function(j,singleRule){
+                if(eleRule[i].slice(0,1) === '!') {
+                    isReverse = 1;
+                    ruleName = eleRule[i].slice(1);
                 }else{
-                    val = r[i];
+                    ruleName = eleRule[i];
                 }
 
-                if(val === v.name){
-                    access = v.validate.call(this,value);
-                    if(!bool && value !='' ){//验证不能空，为空时可能规则是required
+                if(ruleName === singleRule.name){
+                    access = singleRule.validate(value);
+                    if(isReverse && value !=='' ){//不能写!required，不为空取反意思是能为空有病吧
                         access = !access;
                     }
-                    msg = msg ? msg : v.defaultMsg;//有自定义消息则显示自定义消息
-                    if(access){
-                        if(equalto){//验证和指定元素值否相同
+                    msg = msg ? msg : singleRule.defaultMsg;//有自定义消息则显示自定义消息
+                    
+                    if(access){	//验证和指定元素值否相同
+                        if(equalto){
                             if($(equalto).val() != value){
-                                access = false;
+                                access = 0;
                                 msg =  equalmsg; 
                           }
                         }
@@ -72,81 +87,41 @@
 
             });
             
-            if(access) {
-                $(document).data('access',true);//检测是否有一项通过 
-                return false;//必须return false
-            } else{
-                 $(document).data('access',false);
-            }
-            
         });
-
+		
+		//验证后操作
         if(!access){
-            ele.nextAll('.help-block').text(msg);
-            ele.parent().parent().addClass('error');
-            return false;//没通过验证返回false
+           opt.handle(ele,0);
         }else{
-            ele.nextAll('.help-block').text('');
-            ele.parent().parent().removeClass('error');
-            return true;
+           opt.handle(ele,1);
         }
 
     };
 
-    //验证失败时执行
-    var failed = function  () {
-        notice(false,'请正确填写注册信息！');
-    };
-
-    //刷新验证码
-    var refeshVcode = function  (opt) {
-        $('#u-code').click(function  () {
-            $(this).prevAll('img').attr('src',opt.api.vcode+'?'+Math.random());  
-        });
-    };
-
-    var isExist = function  (self,api,obj,msg) {
-        var access = false;
-        $.ajax({
-            type : 'post',
-            url : api,
-            async:false,
-            data : obj,
-            success : function (d) {
-                if(!d.Result){
-                     access = true;
-                }else{
-                    self.defaultMsg = msg || d.Message;
-                    access = false;
-                }
-            }
-        });
-        return access;
-    };
 
    var defaults = {
         validRules:[{
             name: 'required',
             validate: function(value) {
-                return !($.trim(value) === '');
+                return !$.trim(value) === '';
             },
-            defaultMsg: '不能为空'
+            defaultMsg: '字段不能为空'
         }, {
             name: 'number',
             validate: function(value) {
-                return (/^[0-9]\d*$/.test(value));
+                return /^[0-9]\d*$/.test(value);
             },
             defaultMsg: '请输入数字'
         }, {
             name: 'email',
             validate: function(value) {
-                return (/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(value));
+                return /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(value);
             },
             defaultMsg: '请输入正确邮箱地址'
         }, {
             name: 'char',
             validate: function(value) {
-                return (/^[a-z\_\-A-Z]*$/.test(value));
+                return /^[a-z\_\-A-Z]*$/.test(value);
             },
             defaultMsg: '请输入英文字符'
         }, {
@@ -158,72 +133,29 @@
         },{
             name: 'mobile',
             validate: function(value) {
-                return (/^13[0-9]{9}$|14[0-9]{9}|15[0-9]{9}$|18[0-9]{9}$/.test(value));
+                return /^13[0-9]{9}$|14[0-9]{9}|15[0-9]{9}$|18[0-9]{9}$|17[0-9]{9}$/.test(value);
             },
             defaultMsg: '请输入正确手机号码！'  
         },{
             name: 'password',
             validate: function(value) {
-                return (/^[\u4E00-\u9FA5\uf900-\ufa2d\w\.\s]{5,15}$/.test(value));
+                return /^[\u4E00-\u9FA5\uf900-\ufa2d\w\.\s]{5,15}$/.test(value);
             },
             defaultMsg: '密码格式必须为5到15位字符串'      
         },{
             name: 'vcode',
             validate: function(value) {
-                return (/^\d{4}$/.test(value));
+                return /^\d{4}$/.test(value);
             },
             defaultMsg: '验证码必须为4位数字'     
-        },{
-             name: 'mcode',
-            validate: function(value) {
-                return (/^\d{6}$/.test(value));
-            },
-            defaultMsg: '验证码必须为6位数字'
-        },{
-            name: 'validateEmail',
-            validate: function(value) {
-                var self = this , access;
-                self.defaultMsg = '邮箱格式错误';
-                if(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(value)){
-                   return isExist(self,'/ApiClient/IsExistEmail',{Email:value},'邮箱已经存在!');   
-                }
-                return false;
-            },
-            defaultMsg: '邮箱格式错误'     
-         },{
-            name: 'validateMobile',
-            validate: function(value) {
-                var self = this,access;
-                self.defaultMsg = '请输入正确手机号码！';
-                 if(/^13[0-9]{9}$|14[0-9]{9}|15[0-9]{9}$|18[0-9]{9}$/.test(value)){
-                   return isExist(self,'/ApiClient/IsExistMobilePhone',{MobilePhone:value},'手机已经存在!');   
-                }
-                    return false;
-            },
-            defaultMsg: '请输入正确手机号码！'     
-         }, {
-             name: 'validateEmailOrMobile',
-             validate: function (value) {
-                 var self = this, access;
-                 self.defaultMsg = '请输入正确手机号码/邮箱！';
-                 if (/^13[0-9]{9}$|14[0-9]{9}|15[0-9]{9}$|18[0-9]{9}$/.test(value) || /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(value)) {
-                     return isExist(self, '/ApiClient/IsExistEmailOrMobilePhone', { Str: value });
-                 }
-                 return false;
-             },
-             defaultMsg: '请输入正确手机号码！'
-         }],
+        }],
         success : function(){},
-        failed : failed,
-        api : {
-            vcode : '/ApiClient/GetImageCode',//验证码
-            register : '/ApiClient/Register',//注册
-            login : '/ApiClient/Login',//登录
-            modifypass : '/ApiMember/ModifyPassword',//用户中心修改密码
-            findpassword : '/ApiClient/FindPassword'//
-        },
-        addrules : function  (obj) {
-            this.validRules.push(obj);//添加一个自定义规则
+        failed : function(){},
+        refeshCode : function(){},
+        handle : function(){},
+        api : {},
+        addrules : function  (rule) {
+            this.validRules.push(rule);//添加一个自定义规则
         }
     };
 })(jQuery);
